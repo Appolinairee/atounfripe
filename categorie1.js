7292046e1bb32fc9f0a449458782c7f51c5939fa
Appolinairee <100274980+Appolinairee@users.noreply.github.com>
2022-08-11 15:42:34 +0100

    Add files via upload

---
+++ categorie1.js
@@ -0,0 +1,307 @@
+let Tproduits = [
+
+    {
+        natureStock: " Produit ",
+        image1 :"jean7.jpg",
+        image2 :"jean7.jpg",
+        nom :"jEAN noir brillant ",
+        categorie :"Jeans",
+        prixNouveau: '2000F',
+        prixAncien: '2500F'
+    },
+
+    {
+        natureStock: " Produit ",
+        image1 :"troisQuart2.jpg",
+        image2 :"troisQuart2.jpg",
+        nom :"jEAN noir brillant ",
+        categorie :"Jeans",
+        prixNouveau: '2000F',
+        prixAncien: '2500F'
+    },
+
+    {
+        natureStock: "Nouveau",
+        image1: "jean9.jpg",
+        image2 :"jean9.jpg",
+        nom :"Jean bleu homme ",
+        categorie :"Jeans",
+        prixNouveau: '~2000F',
+        prixAncien: '2500F'
+    },
+
+    {
+        natureStock: " New ",
+        image1 :"troisQuart1.jpeg",
+        image2 :"troisQuart2.jpeg",
+        nom :"3/4 Pantalon ",
+        categorie :"3/4",
+        prixNouveau: '1000F',
+        prixAncien: '1200F'
+    },
+
+    {
+    natureStock: "Nouveau",
+    image1: "jean1-1.jpg",
+    image2 :"jean1-2.jpg",
+    nom :"Jean bleu homme ",
+    categorie :"Jeans",
+    prixNouveau: '~1500F',
+    prixAncien: '2500F'
+},
+
+    {
+    natureStock: "Nouveau",
+    image1: "jean1.jpg",
+    image2 :"jean1.jpg",
+    nom :"Jean noir homme ",
+    categorie :"Jeans",
+    prixNouveau: '~1500F',
+    prixAncien: '2500F'
+},
+
+   {
+    natureStock: "Nouveau",
+    image1: "jean6.jpg",
+    image2 :"jean6.jpg",
+    nom :"Jean bleu homme",
+    categorie :"Jeans",
+    prixNouveau: '~2000F',
+    prixAncien: '2500F'
+},
+
+    {
+    natureStock: "Nouveau",
+    image1 :"etalage4.jpg",
+    image2 :"etalage4.jpg",
+    nom :"Catégorie",
+    categorie :"Jeans & Pantalons",
+    prixNouveau: '~~1700F',
+    prixAncien: '2000F'
+},
+
+    {
+        natureStock: " Meilleur ",
+        image1 :"jean4-1.jpg",
+        image2 :"jean4-1.jpg",
+        nom :"Jean cendre ",
+        categorie :"Jeans",
+        prixNouveau: '1500F',
+        prixAncien: '2000F'
+    },
+
+    {
+        natureStock: "Nouveau",
+        image1 :"jean3-2.jpg",
+        image2 :"jean3-1.jpg",
+        nom :"Jeans noir ",
+        categorie :"Jeans",
+        prixNouveau: '2000F',
+        prixAncien: '2500F'
+    },
+
+    {
+        natureStock: "Nouveau",
+        image1 :"jeans.jpg",
+        image2 :"jeans.jpg",
+        nom :"Jeans ",
+        categorie :"Jeans",
+        prixNouveau: '2000F',
+        prixAncien: '2500F'
+    }
+    
+];
+
+let TpeleMeles = [
+    {
+        natureStock: "Meilleur",
+        image1 :"t-shirt5.jpg",
+        image2 :"t-shirt5.jpg",
+        nom :"T-Shirt bleu",
+        categorie :"T-Shirt",
+        prixNouveau: '~1000F',
+        prixAncien: ''
+    },
+   
+    {
+        natureStock: "Meilleur",
+        image1 :"t-shirt3.jpg",
+        image2 :"t-shirt3.jpg",
+        nom :"T-Shirt noir",
+        categorie :"T-Shirt",
+        prixNouveau: '~1000F',
+        prixAncien: ''
+    },
+
+    {
+        natureStock: " Meilleur",
+        image1 :"pull-over.jpg",
+        image2 :"pull-over.jpg",
+        nom :"Pull-over ",
+        categorie :"Pull",
+        prixNouveau: '~1500F',
+        prixAncien: ''
+    },
+
+    {
+        natureStock: "Nouveau",
+        image1 :"chemises.jpg",
+        image2 :"chemises.jpg",
+        nom :"Catégorie",
+        categorie :"Chemises",
+        prixNouveau: '~~1500F',
+        prixAncien: ''
+    },
+    {
+        natureStock: "Nouveau",
+        image1 :"étalage.jpg",
+        image2 :"étalage.jpg",
+        nom :"Pêle-Mêle",
+        categorie :"Catégorie",
+        prixNouveau: '~~1500F',
+        prixAncien: ''
+    },
+
+    {
+        natureStock: "Nouveau",
+        image1 :"pull3-2.jpg",
+        image2 :"pull3-1.jpg",
+        nom :"Pull Cendre",
+        categorie :"Pull Over",
+        prixNouveau: '1000F',
+        prixAncien: '1300F'
+    },
+
+    {
+            natureStock: "Meilleur",
+            image1 :"t-shirt.jpg",
+            image2 :"t-shirt.jpg",
+            nom :"T-Shirt homme",
+            categorie :"T-Shirt",
+            prixNouveau: '1000F',
+            prixAncien: ''
+        },
+    
+    {
+        natureStock: "Produit",
+        image1 :"t-shirt4.jpg",
+        image2 :"t-shirt4.jpg",
+        nom :"t-shirt Bleu",
+        categorie :"t-shirt",
+        prixNouveau: '900F',
+        prixAncien: ''
+    },
+
+    {
+        natureStock: " Produit ",
+        image1 :"t-Shirt7.jpg",
+        image2 :"t-Shirt7.jpg",
+        nom :"t-shirt",
+        categorie :"t-shirt",
+        prixNouveau: '1000F',
+        prixAncien: ''
+    },
+    
+    {
+        natureStock: " Produit ",
+        image1 :"pull1-2.jpg",
+        image2 :"pull1-1.jpg",
+        nom :"Pull-Over ",
+        categorie :"Pull",
+        prixNouveau: '1000F',
+        prixAncien: ''
+    },
+    {
+        natureStock: " Produit ",
+        image1 :"pull-over1.jpg",
+        image2 :"pull-over1.jpg",
+        nom :"Pull-Over ",
+        categorie :"Pull",
+        prixNouveau: '1500F',
+        prixAncien: ''
+    },
+
+    {
+        natureStock: " Nouveau ",
+        image1 :"maillot.jpg",
+        image2 :"maillot.jpg",
+        nom :"Maillot ",
+        categorie :"Habits",
+        prixNouveau: '1000F',
+        prixAncien: ''
+    },
+
+    {
+        natureStock: "Produit",
+        image1 :"t-shirt6.jpg",
+        image2 :"t-shirt6.jpg",
+        nom :"Lacostes",
+        categorie :"t-shirt",
+        prixNouveau: '1000F',
+        prixAncien: ''
+    },
+
+    {
+        natureStock: "Produit",
+        image1 :"pull2-2.jpg",
+        image2 :"pull2-1.jpg",
+        nom :"Pull-over",
+        categorie :"Pull",
+        prixNouveau: '1300F',
+        prixAncien: ''
+    },
+
+    {
+        natureStock: "Meilleur",
+        image1 :"t-shirt1.jpg",
+        image2 :"t-shirt1.jpg",
+        nom :"Lacoste",
+        categorie :"Lacostes",
+        prixNouveau: '~1000F',
+        prixAncien: ''
+    },
+
+    {
+        natureStock: "Meilleur",
+        image1 :"pullover.jpg",
+        image2 :"pullover.jpg",
+        nom :"Pull-over",
+        categorie :"Pull",
+        prixNouveau: '~1500F',
+        prixAncien: ''
+    },
+];
+
+let resultasRecherche = document.querySelector('.resultasRecherche');
+for (let i = 0; i < Tproduits.length; i++) {
+resultasRecherche.innerHTML +=  "<div class='produit'><div class='titleProduit'><p>Jacket</p><h3>Pull Over Homme</h3></div><div class='etoilesProduitC'><i class='fa fa-star'></i><i class='fa fa-star'></i><i class='fa fa-star-half-o'></i><i class='fa fa-star-o'></i><i class='fa fa-star-o'></i></div><div class='imageProduits'><img src='./img/IMG-20211117-WA0072.jpg' alt=''><p><a href='https://wa.me/message/3QWQF7SATXLHM1'><i class='fa fa-whatsapp'></i></a></p></div><p class='angle aVert'>15%</p><div class='optionProduit'><div class='prix'><p class='nouveauPrix'>1500 <span>F</span></p></div> <a href='https://wa.me/message/3QWQF7SATXLHM1'><i class='fa fa-whatsapp'></i></a><i class='fa fa-thumbs-o-up'></i> </div></div>";
+}
+
+var like = document.querySelectorAll('.fa-thumbs-o-up');
+for (let i = 0; i < like.length; i++) {
+like[i].addEventListener('click', function () {
+ like[i].className = ' fa fa-thumbs-up';
+});
+}
+
+let angleHaut = document.querySelectorAll('.angle');
+let produit = document.querySelectorAll('.produit');
+let imageProduits = document.querySelectorAll('.imageProduits');
+let titleProduit = document.querySelectorAll('.titleProduit');
+let nouveauPrix = document.querySelectorAll('.nouveauPrix');
+
+for (let i = 0; i < Tproduits.length; i++) {
+angleHaut[i].innerHTML = Tproduits[i].natureStock;
+
+imageProduits[i].innerHTML ="<img src='./img/" + Tproduits[i].image1 + "' alt='' class='imageProduit1'><p> <i class = 'fa fa-whatsapp'></i> </p>";
+
+titleProduit[i].innerHTML ="<p>" + Tproduits[i].nom + "</p><h3>" + Tproduits[i].categorie + "</h3>";
+  
+nouveauPrix[i].innerHTML ="<p>" + Tproduits[i].prixNouveau + "</p>";
+}
+
+let produitsMinimals  = document.querySelector('.produitsMinimals');
+
+for (let i = 0; i < TpeleMeles.length; i++) { 
+produitsMinimals.innerHTML += "<div class='produitMinimal'><img src='./img/" + TpeleMeles[i].image1 + "' alt=''><div class='produitMinimalTexte'><h4>" + TpeleMeles[i].nom + "</h4><p class='categorieProduit'>" + TpeleMeles[i].categorie + "</p><div class='prix'><p class='nouveauPrix'>" + TpeleMeles[i].prixNouveau + "</p><p class='ancienPrix'>" + TpeleMeles[i].prixAncien + "</p></div></div></div>";
+}
